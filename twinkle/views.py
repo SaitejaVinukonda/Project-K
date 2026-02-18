48ddebd0d466835ac1ed0b772a74b23e59d73d2e
SuryaPrakashBolloju <suryaprakashbolloju@gmail.com>
2026-02-18 15:06:25 +0530

    change

--- twinkle/views.py
+++ twinkle/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def galaxy(request):
     return render(request, 'galaxy/galaxy.html')
+
+
+def galaxy(request):
+    return render(request, 'galaxy/galaxy.html')
